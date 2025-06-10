943ee685822d1269509328262048eb1fa2632745
Alaa Elsayed <alaasayed256354@gmail.com>
2025-06-10 11:44:03 +0300

    ssh key added

--- gitTest/file1.cpp
+++ gitTest/file1.cpp
@@ -5,10 +5,8 @@ void manin() {
 	cout << "non";
 	cout << "are you okay";
 	cout<<"hey soso";
-<<<<<<< HEAD
 	cout << "the commit will be on the branch testing";
 	cout << "second commit in branch testing";
 	cout << "new line in testing master";
-=======
->>>>>>> 005ebc1a6ab942aa91066d227692b81524d7e1ee
+	cout << "After add ssh key";
 }
